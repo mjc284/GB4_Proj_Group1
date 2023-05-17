27a1a37242d29fd2d6d137930a0416ee1ace2f54
mjc284 <mjc284@cam.ac.uk>
2023-05-17 21:46:30 +0100

    Major changes to sender and reciever matlab codes.

--- Sender/commandRecognition.m
+++ Sender/commandRecognition.m
@@ -26,19 +26,19 @@ r = 0;
 
 oldYMode = "background";
 
-arduino = serialport('/dev/cu.usbmodem141401', 9600);
-pause(2);
-write(arduino, 'u', 'char');
-write(arduino, 'p', 'char');
-pause(2);
-write(arduino, 'd', 'char');
-write(arduino, 'o', 'char');
-pause(0.3);
-write(arduino, 'u', 'char');
-write(arduino, 'p', 'char');
-pause(0.3);
-write(arduino, 'd', 'char');
-write(arduino, 'o', 'char');
+%arduino = serialport('/dev/cu.usbserial-1410', 9600);
+%pause(2);
+%write(arduino, 'u', 'char');
+%write(arduino, 'p', 'char');
+%pause(2);
+%write(arduino, 'd', 'char');
+%write(arduino, 'o', 'char');
+%pause(0.3);
+%write(arduino, 'u', 'char');
+%write(arduino, 'p', 'char');
+%pause(0.3);
+%write(arduino, 'd', 'char');
+%write(arduino, 'o', 'char');
 
 while ishandle(h) && toc < timeLimit
 
@@ -86,55 +86,56 @@ while ishandle(h) && toc < timeLimit
         title(string(YMode),'FontSize',20)
     end
 
-    if YMode ~= oldYMode
-        if YMode == "up"
-            write(arduino, 'u', 'char');
-            write(arduino, 'p', 'char');
-            pause(0.300);
-            write(arduino, 'd', 'char');
-            write(arduino, 'o', 'char');
-            pause(0.300);
-            write(arduino, 'u', 'char');
-            write(arduino, 'p', 'char');
-            pause(0.300);
-            write(arduino, 'd', 'char');
-            write(arduino, 'o', 'char');
-            pause(0.3);
-            write(arduino, 'u', 'char');
-            write(arduino, 'p', 'char');
-            pause(0.300);
-            write(arduino, 'd', 'char');
-            write(arduino, 'o', 'char');
-            pause(0.300);
-            write(arduino, 'u', 'char');
-            write(arduino, 'p', 'char');
-            pause(0.300);
-            write(arduino, 'd', 'char');
-            write(arduino, 'o', 'char');
-            pause(0.3);
-        elseif YMode == "down"
-            write(arduino, 'u', 'char');
-            write(arduino, 'p', 'char');
-            pause(0.3);
-            write(arduino, 'd', 'char');
-            write(arduino, 'o', 'char');
-            pause(0.3);
-            write(arduino, 'u', 'char');
-            write(arduino, 'p', 'char');
-            pause(0.3);
-            write(arduino, 'd', 'char');
-            write(arduino, 'o', 'char');
-            pause(0.3);
-        end
-    end
-    oldYMode = YMode;
+    %if YMode ~= oldYMode
+    %    if YMode == "up"
+    %        write(arduino, 'u', 'char');
+    %        write(arduino, 'p', 'char');
+    %        pause(0.300);
+    %        write(arduino, 'd', 'char');
+    %        write(arduino, 'o', 'char');
+    %        pause(0.300);
+    %        write(arduino, 'u', 'char');
+    %        write(arduino, 'p', 'char');
+    %        pause(0.300);
+    %        write(arduino, 'd', 'char');
+    %        write(arduino, 'o', 'char');
+    %        pause(0.3);
+    %        write(arduino, 'u', 'char');
+    %        write(arduino, 'p', 'char');
+    %        pause(0.300);
+    %        write(arduino, 'd', 'char');
+    %        write(arduino, 'o', 'char');
+    %        pause(0.300);
+    %        write(arduino, 'u', 'char');
+    %        write(arduino, 'p', 'char');
+    %        pause(0.300);
+    %        write(arduino, 'd', 'char');
+    %        write(arduino, 'o', 'char');
+    %        pause(0.3);
+    %    elseif YMode == "down"
+    %        write(arduino, 'u', 'char');
+    %        write(arduino, 'p', 'char');
+    %        pause(0.3);
+    %        write(arduino, 'd', 'char');
+    %        write(arduino, 'o', 'char');
+    %        pause(0.3);
+    %        write(arduino, 'u', 'char');
+    %        write(arduino, 'p', 'char');
+    %        pause(0.3);
+    %        write(arduino, 'd', 'char');
+    %        write(arduino, 'o', 'char');
+    %        pause(0.3);
+    %    end
+    %end
+    %oldYMode = YMode;
 
     drawnow
 end
 
-write(arduino, 'u', 'char');
-write(arduino, 'p', 'char');
-pause(2);
-write(arduino, 'd', 'char');
-write(arduino, 'o', 'char');
-
+%write(arduino, 'u', 'char');
+%write(arduino, 'p', 'char');
+%pause(2);
+%write(arduino, 'd', 'char');
+%write(arduino, 'o', 'char');
+%
+%clear(arduino);
