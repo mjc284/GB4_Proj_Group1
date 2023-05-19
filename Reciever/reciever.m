4662451d1076a53b005c38121595886a3fbc4595
mjc284 <mjc284@cam.ac.uk>
2023-05-19 12:28:12 +0100

    Implemented reciever

--- Reciever/reciever.m
+++ Reciever/reciever.m
@@ -10,8 +10,8 @@ h = figure('Units','normalized','Position',[0.2 0.1 0.6 0.8]);
 keyMode = 0; %0 = OOK, 1 = PWM
 
 %two thresholds for trigger hysteresis
-upperThreshold = 4.7;
-lowerThreshold = 4.5;
+upperThreshold = 2;
+lowerThreshold = 1.9;
 
 pulseWidthThreshold = 30;
 
@@ -20,7 +20,7 @@ data = 0; %1 = on, 2 = off
 timer = 0;
 currentState = 0;
 oldState = 0;
-stateSequence = zeros(4);
+stateSequence = zeros(10);
 while ishandle(h) && toc < timeLimit
 
     y = [y(2:end), readVoltage(a,'A0')];
@@ -41,17 +41,16 @@ while ishandle(h) && toc < timeLimit
     end
 
     % Update state sequence history
-    if currentState ~= oldstate || timer > pulseWidthThreshold
+    %if currentState ~= oldState || timer > pulseWidthThreshold
         stateSequence = [stateSequence(2:end), currentState];
-        timer = 0;
-    end
+    %    timer = 0;
+    %end
 
     % Show state sequence history
     subplot(2,1,2);
     plot(stateSequence);
     axis tight;
-    ylim([0,1]);
-    xlim([0,4]);
+    ylim([-1,2]);
     
     % Decode data based on OOK or PWM
     if keyMode == 0
